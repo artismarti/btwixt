489d5777c689d40d6058c4dcea86a825121cc657
artismarti <artismarti@gmail.com>
2019-01-30 19:48:05 +0000

    Misc

--- meetr-api/db/seeds.rb
+++ meetr-api/db/seeds.rb
@@ -1,6 +1,6 @@
 # User.destroy_all
 # Meeting.destroy_all
-UserMeeting.destroy_all
+# UserMeeting.destroy_all
 
 
 # 5.times do
@@ -13,15 +13,21 @@ UserMeeting.destroy_all
 #   Meeting.create!(title: Faker::Nation.capital_city, date_time: Faker::Time.between(DateTime.now + 1, DateTime.now+20))
 # end
 
-UserMeeting.create(user_id: 46, meeting_id: 1, start_latitude: 51.53289 , start_longitude: -0.13096 ,user_status: "created", start_address: "EC2A1AE" )
-UserMeeting.create(user_id: 46, meeting_id: 4, start_latitude: 51.48881 , start_longitude: -0.22293 ,user_status: "invited")
-UserMeeting.create(user_id: 46, meeting_id: 10, start_latitude: 51.52612 , start_longitude: -0.10722 ,user_status: "declined")
+# User.create!(first_name: "Arti", last_name: "Mathanda", email: "artismarti@gmail.com", password: "cat", guest: false)
+# User.create!(first_name: "Megan", last_name: "Folsom", email: "m@f.com", password: "", guest: true)
+# User.create!(first_name: "Graca", last_name: "Morgado", email: "g@m.com", password: "", guest: true)
 
-UserMeeting.create(user_id: 2, meeting_id: 2, start_latitude: 51.53289 , start_longitude: -0.13096 ,user_status: "created", start_address: "W68JS"  )
-UserMeeting.create(user_id: 4, meeting_id: 2, start_latitude: 51.48881 , start_longitude: -0.22293 ,user_status: "invited")
-UserMeeting.create(user_id: 5, meeting_id: 2, start_latitude: 51.48881 , start_longitude: -0.22693 ,user_status: "invited")
-UserMeeting.create(user_id: 11, meeting_id: 2, start_latitude: 51.52612 , start_longitude: -0.10722 ,user_status: "invited")
 
-UserMeeting.create(user_id: 11, meeting_id: 4, start_latitude: 51.53289 , start_longitude: -0.13096 ,user_status: "created", start_address: "N31EY" )
-UserMeeting.create(user_id: 18, meeting_id: 4, start_latitude: 51.48881 , start_longitude: -0.22293 ,user_status: "invited")
-UserMeeting.create(user_id: 19, meeting_id: 4, start_latitude: 51.52612 , start_longitude: -0.10722 ,user_status: "declined")
+UserMeeting.create(user_id: 21, meeting_id: 4, start_latitude: 51.53289 , start_longitude: -0.13096 ,user_status: "created", start_address: "W68JS" )
+UserMeeting.create(user_id: 22, meeting_id: 4, start_latitude: 51.48881 , start_longitude: -0.22293 ,user_status: "invited", start_address: "W68JS" )
+UserMeeting.create(user_id: 23, meeting_id: 4, start_latitude: 51.52612 , start_longitude: -0.10722 ,user_status: "invited", start_address: "W68JS" )
+
+
+# UserMeeting.create(user_id: 2, meeting_id: 2, start_latitude: 51.53289 , start_longitude: -0.13096 ,user_status: "created", start_address: "W68JS"  )
+# UserMeeting.create(user_id: 4, meeting_id: 2, start_latitude: 51.48881 , start_longitude: -0.22293 ,user_status: "invited")
+# UserMeeting.create(user_id: 5, meeting_id: 2, start_latitude: 51.48881 , start_longitude: -0.22693 ,user_status: "invited")
+# UserMeeting.create(user_id: 11, meeting_id: 2, start_latitude: 51.52612 , start_longitude: -0.10722 ,user_status: "invited")
+
+# UserMeeting.create(user_id: 11, meeting_id: 4, start_latitude: 51.53289 , start_longitude: -0.13096 ,user_status: "created", start_address: "N31EY" )
+# UserMeeting.create(user_id: 18, meeting_id: 4, start_latitude: 51.48881 , start_longitude: -0.22293 ,user_status: "invited")
+# UserMeeting.create(user_id: 19, meeting_id: 4, start_latitude: 51.52612 , start_longitude: -0.10722 ,user_status: "declined")
