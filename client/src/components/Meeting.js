42a78606fba35369bd30917a061bb87d54b86772
artismarti <artismarti@gmail.com>
2019-01-30 19:46:44 +0000

    Removes Material UI

--- client/src/components/Meeting.js
+++ client/src/components/Meeting.js
@@ -1,44 +1,5 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import { withStyles } from '@material-ui/core/styles'
-import Card from '@material-ui/core/Card'
-import CardHeader from '@material-ui/core/CardHeader'
-import CardContent from '@material-ui/core/CardContent'
-import CardActions from '@material-ui/core/CardActions'
-import Collapse from '@material-ui/core/Collapse'
-import Avatar from '@material-ui/core/Avatar'
-import IconButton from '@material-ui/core/IconButton'
-import Typography from '@material-ui/core/Typography'
-import red from '@material-ui/core/colors/red'
-import FavoriteIcon from '@material-ui/icons/Favorite'
-import ShareIcon from '@material-ui/icons/Share'
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
-import MoreVertIcon from '@material-ui/icons/MoreVert'
-
-const styles = theme => ({
-  card: {
-    maxWidth: 400,
-  },
-  actions: {
-    display: 'flex',
-  },
-  expand: {
-    transform: 'rotate(0deg)',
-    transition: theme.transitions.create('transform', {
-      duration: theme.transitions.duration.shortest,
-    }),
-    marginLeft: 'auto',
-    [theme.breakpoints.up('sm')]: {
-      marginRight: -8,
-    },
-  },
-  expandOpen: {
-    transform: 'rotate(180deg)',
-  },
-  avatar: {
-    backgroundColor: red[500],
-  },
-})
+import { Card, Image } from 'semantic-ui-react'
 
 class Meeting extends React.Component {
   state = { expanded: false }
@@ -47,49 +8,45 @@ class Meeting extends React.Component {
     this.setState(state => ({ expanded: !state.expanded }))
   }
 
+  getMap = (lat, lng) => {
+    return `https://image.maps.api.here.com/mia/1.6/mapview?app_id=***REMOVED***&app_code=***REMOVED***&lat=${lat}&lon=${lng}&vt=0&z=14`
+  }
+
   render() {
-    const { meeting } = this.props
+    const { meeting, classes } = this.props
 
     return (
-      <Card style={{ margin: '10px' }}>
-        <CardHeader
-          avatar={
-            <Avatar aria-label="Recipe">
-              {meeting.title[0].toUpperCase()}
-            </Avatar>
-          }
-          action={
-            <IconButton>
-              <MoreVertIcon />
-            </IconButton>
-          }
-          title={meeting.title}
-          subheader="September 14, 2016"
-        />
-        <CardActions disableActionSpacing>
-          <IconButton aria-label="Add to favorites">
-            <FavoriteIcon />
-          </IconButton>
-          <IconButton aria-label="Share">
-            <ShareIcon />
-          </IconButton>
-
-          <ExpandMoreIcon />
-        </CardActions>
-        <Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
-          <CardContent>
-            <Typography paragraph variant="body2">
-              {meeting.datetime}
-            </Typography>
-          </CardContent>
-        </Collapse>
-      </Card>
+      <React.Fragment>
+        <Card color="orange">
+          <Card.Content>
+            <Card.Header>
+              {meeting.title}: {meeting.date_time}
+            </Card.Header>
+            <ul>
+              Invitees:
+              {meeting.users.map(u => (
+                <li key={`${u.id}_user`}>
+                  {u.first_name} {u.last_name}
+                </li>
+              ))}
+            </ul>
+            <ul>
+              Start Address:
+              {meeting.userMeetings.map(um => (
+                <li key={um.id}>{um.start_address}</li>
+              ))}
+            </ul>
+          </Card.Content>
+          <Image
+            src={this.getMap(
+              meeting.midpoint_latitude,
+              meeting.midpoint_longitude
+            )}
+          />
+        </Card>
+      </React.Fragment>
     )
   }
 }
 
-Meeting.propTypes = {
-  classes: PropTypes.object.isRequired,
-}
-
-export default withStyles(styles)(Meeting)
+export default Meeting
