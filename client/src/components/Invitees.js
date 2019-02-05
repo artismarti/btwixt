747362dd396b49665f7eb278de1aa0ffdd23952d
artismarti <artismarti@gmail.com>
2019-02-05 18:58:21 +0000

    Moves Meeting Invitees into separate component

---
+++ client/src/components/Invitees.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Label, Segment, List } from 'semantic-ui-react'
+
+class Invitees extends React.Component {
+  iconSelector = status => {
+    switch (status) {
+      case 'invited':
+        return <List.Icon name="help circle" color="grey" />
+        break
+      case 'accepted':
+        return <List.Icon name="thumbs up outline" color="green" />
+        break
+      case 'declined':
+        return <List.Icon name="thumbs down outline" color="red" />
+        break
+      case 'created':
+        return <List.Icon name="user circle" color="blue" />
+        break
+      default:
+        return <List.Icon name="help" color="red" />
+    }
+  }
+  render() {
+    const { guests, email } = this.props
+    return (
+      <Segment raised>
+        <List>
+          <Label as="a" color="olive" ribbon>
+            Invitees:
+          </Label>
+          {guests
+            .filter(g => g.email !== email)
+            .map(g => (
+              <List.Item key={g.id}>
+                {this.iconSelector(g.user_status)}
+                {g.first_name} {g.last_name}: {g.start_address}
+              </List.Item>
+            ))}
+        </List>
+      </Segment>
+    )
+  }
+}
+
+export default Invitees
