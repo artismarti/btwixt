6d400164d73c0abc74114222443c5772e952c65d
artismarti <artismarti@gmail.com>
2019-02-05 16:32:47 +0000

    Calculates lat long from address

--- meetr-api/app/models/user_meeting.rb
+++ meetr-api/app/models/user_meeting.rb
@@ -1,4 +1,17 @@
+include Math
+require 'rest-client'
+
 class UserMeeting < ApplicationRecord
   belongs_to :user
   belongs_to :meeting
+
+def get_lat_lng(address)
+    url = "https://geocoder.api.here.com/6.2/geocode.json?app_id=EUNJEIDbEAKKaUz5IRBj&app_code=nI-30KLhGkA-zFavU7hhYw&searchtext=#{address}"
+    response_string = RestClient.get(url)
+    response_hash = JSON.parse(response_string)
+    latitude = response_hash["Response"]["View"][0]["Result"][0]["Location"]["DisplayPosition"]["Latitude"]
+    longitude = response_hash["Response"]["View"][0]["Result"][0]["Location"]["DisplayPosition"]["Longitude"]
+    self.update(start_latitude: latitude, start_longitude: longitude)
+end
+
 end
