4fd3bd8f012836168332dbdc6b501e23479f159d
artismarti <artismarti@gmail.com>
2019-02-05 16:30:48 +0000

    Adds controller method to update guest status & recalculate meeting midpoint

--- meetr-api/app/controllers/user_meetings_controller.rb
+++ meetr-api/app/controllers/user_meetings_controller.rb
@@ -1,2 +1,16 @@
 class UserMeetingsController < ApplicationController
+
+	def update
+		@guest = current_user.id
+		@meeting = Meeting.find(params["meeting"])
+		@user_meeting = UserMeeting.find_by(
+			:user_id => @guest,
+			:meeting_id => @meeting
+			)
+		@user_meeting.update(:user_status => params["decision"])
+		# @user_meeting.get_lat_lng(params["startLocation"])
+    @meeting.recalculate_midpoint
+    render json: {message: "Updated Midpoint"}
+	end
+
 end
