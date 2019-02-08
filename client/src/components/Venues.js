6c6f141402d41fdce3189a508dc5baf8c5f0ecfc
artismarti <artismarti@gmail.com>
2019-02-08 11:08:06 +0000

    Refactors Venue to show on pop up

--- client/src/components/Venues.js
+++ client/src/components/Venues.js
@@ -1,19 +1,30 @@
 import React from 'react'
-import { Label, Segment, List } from 'semantic-ui-react'
+
+import { Label, List, Button, Popup, Segment } from 'semantic-ui-react'
 
 class Venues extends React.Component {
   render() {
     const { venues } = this.props
     return (
-      <Segment raised>
-        <List>
-          <Label attached="top" color="blue">
-            Venues:
-          </Label>
-          {venues.map(v => (
-            <List.Item key={v.id}>{v.name}</List.Item>
-          ))}
-        </List>
+      <Segment>
+        <Popup
+          trigger={
+            <Button fluid color="blue">
+              View Suggested Venues
+            </Button>
+          }
+          flowing
+          hoverable
+        >
+          <List>
+            <Label attached="top" color="blue">
+              Venues:
+            </Label>
+            {venues.map(v => (
+              <List.Item key={v.id}>{v.name}</List.Item>
+            ))}
+          </List>
+        </Popup>
       </Segment>
     )
   }
