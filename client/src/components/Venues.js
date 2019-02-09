18b77a2dfb7abbfb36c1620d178691be9655faf5
artismarti <artismarti@gmail.com>
2019-02-09 19:05:02 +0000

    Refactor Venues

--- client/src/components/Venues.js
+++ client/src/components/Venues.js
@@ -1,31 +1,33 @@
 import React from 'react'
 
-import { Label, List, Button, Popup, Segment } from 'semantic-ui-react'
+import { Label, List, Message, Popup } from 'semantic-ui-react'
 
 class Venues extends React.Component {
   render() {
     const { venues } = this.props
     return (
-      <Segment>
-        <Popup
-          trigger={
-            <Button fluid color="blue">
-              View Suggested Venues
-            </Button>
-          }
-          flowing
-          hoverable
-        >
-          <List>
-            <Label attached="top" color="blue">
-              Venues:
-            </Label>
-            {venues.map(v => (
-              <List.Item key={v.id}>{v.name}</List.Item>
-            ))}
-          </List>
-        </Popup>
-      </Segment>
+      <Popup
+        trigger={
+          <Message
+            size="mini"
+            fluid
+            icon="food"
+            header="Suggested Venues"
+            info
+          />
+        }
+        flowing
+        hoverable
+      >
+        <List>
+          <Label attached="top" color="blue">
+            Venues:
+          </Label>
+          {venues.map(v => (
+            <List.Item key={v.id}>{v.name}</List.Item>
+          ))}
+        </List>
+      </Popup>
     )
   }
 }
