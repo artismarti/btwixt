a879d9253bfa33a6cea600e45258695d0e580b3c
artismarti <artismarti@gmail.com>
2019-02-09 19:07:15 +0000

    Lets new user not have first or last name

--- meetr-api/app/models/user.rb
+++ meetr-api/app/models/user.rb
@@ -2,6 +2,4 @@ class User < ApplicationRecord
   has_many :user_meetings, dependent: :delete_all
   has_many :meetings, through: :user_meetings
   has_secure_password validations: false
-  validates :first_name, presence: true
-  validates :last_name, presence: true
 end
