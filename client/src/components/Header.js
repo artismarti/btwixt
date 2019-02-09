348d29f8e56df5d2ec0a6bd2f31d8d61a7a7f556
artismarti <artismarti@gmail.com>
2019-02-09 19:03:28 +0000

    Refactor Header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import { Button } from 'semantic-ui-react'
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap/'
 
 class Header extends Component {
   state = {
@@ -18,40 +19,63 @@ class Header extends Component {
     const { email, signout } = this.props
     return (
       <header className="App-header">
-        <h1 className="App-title">
-          {email ? `Welcome back, ${email}` : `Welcome to Meetr.`}
-          <br />
-          {email ? (
-            <>
-              <button onClick={signout}>SIGN OUT</button>
-              <Link to="/create">
-                <Button positive>Create Event</Button>
-              </Link>
-              <Button onClick={() => this.handleClick('invited')}>
-                Pending Invites
-              </Button>
-              <Button onClick={() => this.handleClick('accepted')}>
-                Accepted Events
-              </Button>
-              <Button onClick={() => this.handleClick('declined')}>
-                Declined Events
-              </Button>
-              <Button onClick={() => this.handleClick('')}>All Events</Button>
-              <Button onClick={() => this.handleClick('created')}>
-                My Events
-              </Button>
-              <Button
-                onClick={() => this.handleUpcoming(this.state.dateFilter)}
-              >
-                Upcoming Events
-              </Button>
-            </>
-          ) : (
-            <Link to="/signin">
-              <button>SIGN IN</button>
-            </Link>
-          )}
-        </h1>
+        <Navbar fixed="top" bg="warning" variant="dark">
+          <Navbar.Brand href="#home">bTwixt</Navbar.Brand>
+          <Link to="/signin">
+            <button>SIGN IN</button>
+          </Link>
+        </Navbar>
+        <br />
+        {email && (
+          <>
+            <Navbar fixed="top" bg="dark" expand="lg" variant="dark">
+              <Navbar.Brand href="#home">bTwixt</Navbar.Brand>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                  <Link to="/create">
+                    <Button positive>Create Event</Button>
+                  </Link>
+                  <NavDropdown title="Filter" id="basic-nav-dropdown">
+                    <NavDropdown.Item
+                      onClick={() => this.handleClick('created')}
+                    >
+                      Events I Created
+                    </NavDropdown.Item>
+                    <NavDropdown.Item
+                      onClick={() => this.handleClick('accepted')}
+                    >
+                      Accepted Events
+                    </NavDropdown.Item>
+                    <NavDropdown.Item
+                      onClick={() => this.handleClick('declined')}
+                    >
+                      Declined Events
+                    </NavDropdown.Item>
+                    <NavDropdown.Item
+                      onClick={() => this.handleClick('invited')}
+                    >
+                      Pending Invitations
+                    </NavDropdown.Item>
+                    <NavDropdown.Item
+                      onClick={() => this.handleUpcoming(this.state.dateFilter)}
+                    >
+                      Upcoming Events
+                    </NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item onClick={() => this.handleClick('')}>
+                      All Events
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                </Nav>
+                <Navbar.Text className="mr-sm-2 header">
+                  {email ? `Welcome back, ${email}` : `Welcome to bTw📍xt.`}
+                </Navbar.Text>
+                <button onClick={signout}>SIGN OUT</button>
+              </Navbar.Collapse>
+            </Navbar>
+          </>
+        )}
       </header>
     )
   }
