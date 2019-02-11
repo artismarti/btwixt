43b36f1d96a49161897ad5c1d838428aafbd9082
artismarti <artismarti@gmail.com>
2019-02-11 10:49:20 +0000

    Adds Landing page

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,28 +1,53 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Carousel, Image } from 'react-bootstrap/'
 
 const HomePage = props => (
-  <div>
-    <h1>Home Page!</h1>
-    <p>We currently have the following routes:</p>
-    <p>
-      <Link to="/">
-        <b>HomePage:</b>
-      </Link>
-      <b> '/'</b>
-    </p>
-    <p>
-      <Link to="/signin">
-        <b>SignInForm:</b>
-      </Link>
-      <b> '/signin'</b>
-    </p>
-    <p>
-      <Link to="/create">
-        <b>Create a Meeting:</b>
-      </Link>
-      <b> '/create'</b>
-    </p>
-  </div>
+  <Carousel fade keyboard>
+    <Carousel.Item>
+      <Image
+        className="d-block w-100"
+        src="https://images.unsplash.com/photo-1462536943532-57a629f6cc60?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60"
+        alt="First slide"
+        fluid
+      />
+      <Carousel.Caption>
+        <h3>Meet in the middle</h3>
+        <p>
+          bTwixt makes it easy to decide where to meet friends starting off from
+          different locations.
+        </p>
+      </Carousel.Caption>
+    </Carousel.Item>
+    <Carousel.Item>
+      <img
+        className="d-block w-100"
+        src="https://images.unsplash.com/photo-1506869640319-fe1a24fd76dc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+        alt="Second slide"
+      />
+
+      <Carousel.Caption>
+        <h3>No more arguments about where to meet</h3>
+        <p>
+          Just tell bTwixt all the starting locations & get back suggested
+          location and venues to meet at.
+        </p>
+      </Carousel.Caption>
+    </Carousel.Item>
+    <Carousel.Item>
+      <img
+        className="d-block w-100"
+        src="https://images.unsplash.com/photo-1496275068113-fff8c90750d1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+        alt="Third slide"
+      />
+
+      <Carousel.Caption>
+        <h3>Focus on the friendship, not the logistics</h3>
+        <p>
+          Easily invite friends, get updated suggestions if someone drops out or
+          joins in.
+        </p>
+      </Carousel.Caption>
+    </Carousel.Item>
+  </Carousel>
 )
 export default HomePage
